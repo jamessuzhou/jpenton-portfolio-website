de5ca22db5d993b59d8c9e50acac84fc036c0e2c
James Penton <jamespenton@live.co.uk>
2023-06-06 13:08:21 +0100

    picture fiddling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,15 +11,13 @@ const Hero = () => {
             <div className="col-md-8">
               <h5 className="card-title">James</h5>
               <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                Hello hello hello hello
               </p>
               <p className="text text-muted">Last updated 3 mins</p>
             </div>
             <div className="col-md-4">
               <img
-                src="../../src/images/robot.jpg"
+                src="../../src/images/robot.jpeg"
                 className="img-fluid rounded-start"
                 alt="..."
               />
