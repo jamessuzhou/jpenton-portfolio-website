1033e00ccbef90794d78c74a7a47d8f3faf36f29
James Penton <jamespenton@live.co.uk>
2023-06-06 12:08:42 +0100

    fixing rollupoptions hopefully

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      external: [/^node:.*/],
+    },
+  },
 });
